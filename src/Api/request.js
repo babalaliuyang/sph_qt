b9331e97fa28840d56054b0ed6b3f2109c6a3214
1559299743@qq.com <1559299743@qq.com>
2023-04-20 15:35:07 +0800

    master

--- src/Api/request.js
+++ src/Api/request.js
@@ -4,7 +4,8 @@ import nProgress from "nprogress";
 import "nprogress/nprogress.css";
 // console.log('requert.js', nProgress);
 // start: 进度条开始  done: 进度条结束
-
+// 在当前模块中引入store
+import store from "@/store";
 // 对于axios进行二次封装
 import axios from "axios";
 // 1.利用axios对象的方法create，去创建一个axios实例
@@ -19,6 +20,11 @@ const requests = axios.create({
 // 请求拦截器：再发请求之前，请求拦截器可以检测到；可以在请求发出去之前做一些事
 requests.interceptors.request.use((config) => {
     // config:配置对象，对象里面有一个属性很重要，hearders请求头
+    // console.log(store);
+    if (store.state.detail.uuid_token) {
+        // 给请求头添加一个字段(userTempId)，和后台商量好
+        config.headers.userTempId = store.state.detail.uuid_token
+    }
     nProgress.start();
     return config
 })
@@ -26,7 +32,7 @@ requests.interceptors.request.use((config) => {
 // 响应拦截器
 requests.interceptors.response.use((res) => {
     // 成功的回调函数：服务器响应数据回来以后，相应拦截器可以检测到，可以做一些事情
-    nProgress.done()        
+    nProgress.done()
     return res.data
 }, (error) => {
     // 响应失败的回调
--- src/utils/uuid_token.js
+++ src/utils/uuid_token.js
@@ -1,5 +1,11 @@
 import { v4 as uuidv4 } from 'uuid'
 // 要生成一个随机字符串，且该字符串每次执行时不发生变化，游客身份持久储存
 export const getUUID = () => {
+    let uuid_token = localStorage.getItem('UUIDTOKEN')
+    if (!uuid_token) {
+        uuid_token = uuidv4()
+        localStorage.setItem('UUIDTOKEN', uuid_token)
+    }
 
+    return uuid_token
 }
